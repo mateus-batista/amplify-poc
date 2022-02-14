36a3838681fba19c06f37f0a86ed9a82a56d6375
Mateus Batista <mateusalfredobatista@gmail.com>
2022-02-14 13:27:13 -0300

    added api

---
+++ pages/name.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home({ data }) {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Amplify POC</title>
+        <meta name="description" content="Amplify POC" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Name from api: {data.name}</h1>
+      </main>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${process.env.DOMAIN_URL}/api/hello`)
+  const data = await res.json()
+
+  return { props: { data } }
+}
